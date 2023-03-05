5e9e3787d70dee1e8edeba86227f20b2c38e69e4
Thukten Singye <thuktensingye2163@gmail.com>
2023-03-05 14:02:42 +0600

    upload image to firebase storage

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,8 +1,8 @@
 
 import { useState , useEffect} from "react"
-import { projectAuth} from "../firebase/config"
+import { projectAuth,projectStorage} from "../firebase/config"
 import {createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
-
+import {ref, uploadBytes, getDownloadURL} from 'firebase/storage'
 
 import useAuthContext from "./useAuthContext"
 
@@ -12,21 +12,33 @@ const useSignup = ()=>{
     const [isPending, setIsPending] = useState(false)
     const {dispatch} = useAuthContext()
 
-    const signup = async (email, password, displayName)=>{
+    const signup = async (email, password, displayName, thumbnail)=>{
         setError(null) // reset error everytime we sign in
         setIsPending(true)
         await createUserWithEmailAndPassword(projectAuth, email, password)
         .then((userCredential) => {
             // Handle successful account creation
             const user = userCredential.user
-            updateProfile(user, {displayName}).then((result)=>{ // used to update profile
+
+            // upload user thumbnai on the following path 
+            const uploadPath = `thumbnails/${user.uid}/${thumbnail.name}`
+            const storageRef = ref(projectStorage, thumbnail)
+
+            const imgURL = getDownloadURL(storageRef) // it will get user id
+            // upload image
+            uploadBytes(storageRef, uploadPath).then((snapshot)=>{
+                console.log("Upload image ")
+            })
+
+            updateProfile(user, {displayName, photoURL: imgURL}).then((result)=>{ // used to update profile
+            
             }).catch(err=>{
                 setError(err.message)
             })
             // disptach login function
             dispatch({type:'LOGIN', payload:user})
-            if (!isCancelled){
 
+            if (!isCancelled){
                 setIsPending(false)
                 setError(null)
             }
