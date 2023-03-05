c979e7e0cb262d44841ded7ca920059d70db0d63
Thukten Singye <thuktensingye2163@gmail.com>
2023-03-05 17:35:51 +0600

    created user document

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,45 +1,60 @@
 
 import { useState , useEffect} from "react"
-import { projectAuth,projectStorage} from "../firebase/config"
+import { projectAuth,projectStorage, projectFirestore} from "../firebase/config"
 import {createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
 import {ref, uploadBytes, getDownloadURL} from 'firebase/storage'
-
+// import use context hook
 import useAuthContext from "./useAuthContext"
+
+import { collection , doc, setDoc} from "firebase/firestore"
 
 const useSignup = ()=>{
     const [isCancelled, setIsCancelled] = useState(false)
     const [error, setError] = useState(null)
     const [isPending, setIsPending] = useState(false)
     const {dispatch} = useAuthContext()
-    // const [imgURL, setImageURL]  = useState(null)
+    // const [imgURL, setImgURL] = useState(null)
+
     const signup = async (email, password, displayName, thumbnail)=>{
         setError(null) // reset error everytime we sign in
         setIsPending(true)
-        await createUserWithEmailAndPassword(projectAuth, email, password)
-        .then((userCredential) => {
-            // Handle successful account creation
-            const user = userCredential.user
+        try{
+            const res = await createUserWithEmailAndPassword(projectAuth, email, password)
+            const user = res.user
 
-            // upload user thumbnai on the following path 
+            // .then((userCredential) => {
+                // Handle successful account creation
+                // const user = userCredential.user
+                // upload user thumbnai on the following path 
             const uploadPath = `thumbnails/${user.uid}/${thumbnail.name}`
             const imgRef = ref(projectStorage, uploadPath)
-
+            
             // upload image
-            uploadBytes(imgRef, thumbnail).then((snapshot)=>{
-                getDownloadURL(imgRef).then((url)=>{    
-                    updateProfile(user, {displayName, photoURL:url}).then((result)=>{ // used to update profile
+            await uploadBytes(imgRef, thumbnail).then((snapshot)=>{
+                setError(null)
+            
+                }).catch(err=>{
+                    setError(err.message)
+                })
 
-                    }).catch(err=>{
-                        setError(err.message)
-                    })
-                
-                    })
-            }).then(()=>{
+            const imgURL = await  getDownloadURL(imgRef)
+            await  updateProfile(user, {displayName, photoURL:imgURL}).then((result)=>{
                 setError(null)
             }).catch(err=>{
                 setError(err.message)
-            })
-
+            })                                                                                      // used to update profile
+    
+            
+            // create user document this will show user with theri avatar and whether they are online or not 
+            const collectionRef = collection(projectFirestore, 'user') // get collection
+            const docRef = doc(collectionRef,user.uid) // get document referece from collection
+            setDoc(docRef,{
+                online: true,
+                displayName,
+                photoURL: imgURL
+            } ) // set new doc based on doc reference
+            
+            
             // disptach login function
             dispatch({type:'LOGIN', payload:user})
 
@@ -47,19 +62,18 @@ const useSignup = ()=>{
                 setIsPending(false)
                 setError(null)
             }
-        })
-        .catch((error) => {
-            // Handle errors
-            console.error(error)
-            if (!isCancelled){
-                setError(error)
-                setIsPending(false)
-            }
-        })
+        
+            
+        }catch(err){
+            setError(err.message)
+        }
     }
+
+
     useEffect(()=>{
         return () => setIsCancelled(true)
     },[])
+
 
     return {error, isPending, signup}
 }
