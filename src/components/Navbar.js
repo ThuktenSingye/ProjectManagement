5bb730bc2be4453746bbc3ab80d223ae2193c38e
Thukten Singye <thuktensingye2163@gmail.com>
2023-03-06 15:17:14 +0600

    set condition to show navbar based on user state

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,13 @@ import "./Navbar.css"
 import Temple from "../assets/temple.svg"
 // import logout hooks
 import useLogOut from '../hooks/useLogOut'
+// import context to get user sttae
+import useAuthContext from '../hooks/useAuthContext'
+
 function Navbar() {
-  
+
   const {logout, isPending} = useLogOut()
+  const {user} = useAuthContext()
   return (
     <div className='navbar'>
         <ul>
@@ -15,12 +19,19 @@ function Navbar() {
                 <img src={Temple} alt="logo" />
                 <span>The Singye</span>
             </li>
-            <li><Link to='/login'>Login</Link></li>
-            <li><Link to='/signup'>SignUp</Link></li>
+            {!user && (
+              <>
+              {/* we used fragment here beacuse we have more than one children to return */}
+                <li><Link to='/login'>Login</Link></li>
+                <li><Link to='/signup'>SignUp</Link></li>
+              </>
+            )}
+            {user && (
             <li>
                 {!isPending && <button className='btn' onClick={logout}>Logout</button>}
                 {isPending && <button className='btn' disabled>Logging out ..</button>}
             </li>
+            )}
         </ul>
     </div>
   )
