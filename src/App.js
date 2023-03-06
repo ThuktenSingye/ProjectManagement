328952d9211f525439b7dd4732890442347379cd
Thukten Singye <thuktensingye2163@gmail.com>
2023-03-06 15:06:32 +0600

    added redirect based on whether user is available or new

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import {BrowserRouter, Route, Routes } from 'react-router-dom'
+import {BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import './App.css';
+
 // import pages and component
 import Create from './pages/create/Create';
 import Dashboard from './pages/dashboard/Dashboard';
@@ -9,23 +10,30 @@ import Signup from './pages/signup/Signup';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import React from 'react';
+// import context
+import useAuthContext from './hooks/useAuthContext';
 
 function App() {
+  // based on user state like if user is null or is login, we can redirect to different component
+  const {user, authIsReady} = useAuthContext()
+
   return (
     <div className="app">
+      {authIsReady && (
         <BrowserRouter>
           <Sidebar/>
             <div className="container">
               <Navbar />
               <Routes>
-                <Route exact="true" path='/' element={ <Dashboard />}/> 
-                <Route path='/create' element={ <Create />}/>
-                <Route path='/login' element={ <Login />}/>
-                <Route path='/project' element={ <Project />}/>
-                <Route path='/signup' element={ <Signup />}/>    
+                <Route exact="true" path='/' element={user ? <Dashboard/>: <Navigate to='/login'/>} />
+                <Route path='/create' element={user ? <Create /> : <Navigate to='/login'/>}/>
+                <Route path='/login' element={user?<Navigate to='/'/> :<Login />}/>
+                <Route path='/projects/:id' element={user? <Project />: <Navigate to='/login'/>}/>
+                <Route path='/signup' element={ user ? <Navigate to='/'/>:<Signup />}/>    
               </Routes>
             </div>
         </BrowserRouter>
+      )}
     </div>
   );
 }
